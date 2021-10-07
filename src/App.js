1b2879e7597c32c1f41993c1b97c5fc02a5f2b49
henrik <henrik501@gmail.com>
2021-10-08 00:06:29 +0800

    implement number box

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from "react"
-import { CustomInputNumber } from "./components/CustomInputNumber/CustomInputNumber";
+import CustomInputNumber from "./components/CustomInputNumber/CustomInputNumber";
 
-function App() {
+export default function App() {
+
+    function greetUser() {
+        console.log("Hi there, user!");
+      }
+
     return (<div>
         <h2>Welcome to React App</h2>
         <h3>Date : {new Date().toDateString()}</h3>
-        <CustomInputNumber />
+        <div className="room-manager111">
+            <CustomInputNumber value={2} disabled={true}/>
+        </div>
+        <div>
+            <p>Click this text to see the event bubbling</p>     
+            <button onClick={greetUser}>Click me</button>   
+        </div>
     </div>)
 }
-
-export default App
--- src/components/CustomInputNumber/CustomInputNumber.js
+++ src/components/CustomInputNumber/CustomInputNumber.js
@@ -1,12 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types'
 import "./CustomInputNumber.css";
 
-export function CustomInputNumber() {
+function CustomInputNumber(props) {
+    let { min, max, step} = props;
+    const [counter, setCounter] = useState(props.value || 0);
+
+    function handleIncrease(event) {
+        if(max && max < counter+step) {
+            setCounter(max);
+            return;
+        }
+        console.log(counter+step);
+        setCounter(counter+step);
+    }
+    
+    function handleDecrease(event) {
+        if(min && min > counter-step) {
+            setCounter(min);
+            return;
+        }
+        setCounter(counter-step);
+    }
+
+    function handleOnChange(event){
+        if(props.onChange) {
+            props.onChange();
+        }
+        
+        console.log(`call on chage ${event.target.value}`);
+        setCounter( parseInt(event.target.value, 10));
+    }
+
     return (
         <div className="number-box">
-            <button >-</button>
-            <input type="number" />
-            <button >+</button>
+            <button onMouseDown={handleDecrease} disabled={props.disabled}> - </button>
+            <input type="number" value={counter} step={step} onChange={handleOnChange} disabled={props.disabled} />
+            <button onMouseDown={handleIncrease} disabled={props.disabled} > + </button>
         </div>
     )
 }
+
+CustomInputNumber.propTypes = {
+    min: PropTypes.number, 
+    max: PropTypes.number, 
+    step: PropTypes.number, 
+    name: PropTypes.string, 
+    value: PropTypes.number, 
+    onChange: PropTypes.func, 
+    onBlur: PropTypes.func, 
+    disabled: PropTypes.bool
+}
+
+CustomInputNumber.defaultProps = {
+    min: 0, 
+    step: 1,
+    value: 1,
+    disabled: false
+}
+
+export default CustomInputNumber
